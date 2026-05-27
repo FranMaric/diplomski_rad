17a72fac7189209d18dc2e8ae68116f551636434
Fran Maric <fran.maric.vk@gmail.com>
2026-05-27 15:16:33 +0200

    add inference bag recording

---
+++ control-example/record_inference_bag.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+
+BAG_NAME="/bags/inference_$(date +%Y%m%d_%H%M%S)"
+
+rosbag record \
+  /joint_states \
+  /ee_camera/image_raw \
+  /scene_camera/image_raw \
+  /cartesian_pose_controller/equilibrium_pose \
+  /optoforce_0 \
+  /tf \
+  /tf_static \
+  /action_chunk_vis \
+  -O "$BAG_NAME"
--- control-example/run_docker.sh
+++ control-example/run_docker.sh
@@ -3,6 +3,7 @@ docker run -it --init \
   --rm \
   --name control-example-node \
   --network host \
+  -v /home/rf-blue/Desktop/fran_ws/control-example/bags:/bags \
   --device /dev/video0 \
   --device /dev/video2 \
   --device /dev/ttyACM0 \
