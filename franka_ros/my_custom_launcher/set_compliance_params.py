1a2529a31e113ecc96168b133743a1e018e5e640
Fran Maric <fran.maric.vk@gmail.com>
2026-05-28 12:27:26 +0200

    Fix set compliance script

--- franka_ros/my_custom_launcher/set_compliance_params.py
+++ franka_ros/my_custom_launcher/set_compliance_params.py
@@ -5,8 +5,8 @@ import dynamic_reconfigure.client
 rospy.init_node('set_compliance_params')
 
 client = dynamic_reconfigure.client.Client(
-    '/cartesian_impedance_controller/dynamic_reconfigure_compliance_param_node',
-    timeout=30
+    '/cartesian_impedance_controllerdynamic_reconfigure_compliance_param_node',
+    timeout=10
 )
 client.update_configuration({
     'translational_stiffness': 5000.0,
